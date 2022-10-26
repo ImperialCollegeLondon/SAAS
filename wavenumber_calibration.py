0b13cd270402f4051e1780bdd4de81eba84b0582
ChristianClear <christian.clear@imperial.ac.uk>
2022-10-26 11:48:20 +0100

    feat: added statistical uncertainty function

---
+++ wavenumber_calibration.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calc_stat_unc(fwhm, res, snr): 
+    """
+    Calculates the statistical uncertainty of a voigt fitted spectral line. Formula from: Craig J. Sansonetti and Gillian Nave 2014 ApJS 213 28.
+    
+    Inputs:
+    fwhm: full width at half maximum of the line (in mK)
+    res: resolution of the spectrum
+    snr: signal to noise ratio of the spectral line
+    """ 
+    
+    return np.sqrt(fwhm * res) / snr  # Same as: FWHM / (np.sqrt(Number of independant points across FWHM) * SNR)
