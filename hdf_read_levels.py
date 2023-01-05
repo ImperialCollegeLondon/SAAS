430d3341ded37c959ca93c6e7fe19f389db2683c
gnave <gilliannave@gmail.com>
2023-01-05 12:47:09 -0500

    feat: Test the LineSpec class

--- hdf_read_levels.py
+++ hdf_read_levels.py
@@ -1,13 +1,13 @@
 import h5py
-from branching_fraction import EnergyLevel, Spectrum
+from branching_fraction import EnergyLevel, Spectrum, LineSpec
 
 
 """ 
 Test program to read energy levels from hdf5 file
 """
 
-def getlevs():
-    with h5py.File('Cr_BF2.h5', 'r') as f: 
+def getlevs(fname):
+    with h5py.File(fname, 'r') as f: 
         levels = f['Levels/Cr II levels']
         
         # Convert levels from a list of dictionaries to a list of EnergyLevel objects
@@ -32,4 +32,16 @@ def getspec(fname):
     
     return()
 
-getspec('Cr_BF2.h5')
+def getline(fname):
+    with h5py.File(fname,'r+') as f:
+        lin = f['Spectra/Cr110600.001/intensity calibrated linelists/Cr110600.001.wintcorr']
+        spec = Spectrum( f['Spectra/Cr110600.001/spectrum/spectrum'] )
+
+        lines = [LineSpec(i,spec) for i in lin ]
+        for i in lines:
+            print(i.sig, i.xint, i.width, i.dmping, i.ew, i.npts, i.inten_unc, i.sig_statunc)
+
+
+getlevs('Cr_BF2.h5')
+#getspec('Cr_BF2.h5')
+# getline('Cr_BF2.h5')
