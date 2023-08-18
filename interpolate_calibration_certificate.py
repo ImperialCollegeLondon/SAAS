b300108b8db08acbc52124691483b11c9e40b0c3
gnave <gilliannave@gmail.com>
2023-08-18 16:26:59 +0100

    continue pseudocode for interpolation

--- interpolate_calibration_certificate.py
+++ interpolate_calibration_certificate.py
@@ -1,4 +1,6 @@
 from scipy.interpolate import interp 
+import matplotlib.pyplot as plt
+
 from WavenumberConversions import airtovac
 
 def InterpolateCalibration(calibration_certificate, wstart, wstop, delw):
@@ -11,16 +13,31 @@ def InterpolateCalibration(calibration_certificate, wstart, wstop, delw):
 
     converted_certificate = ConvertToPhotonsPerWavenumber(calibration_certificate)
 
-    waveno = np.linspace()    # Define new wavenumber scale for calibration
-  
     """
        Cubic spline interpolation should be used. First define the grid for the interpolation
        Try using interpn for cubic interpolation
     """
 
     xi = np.linspace(wstart, wstop, num=(wstart-wstop)/delw+1)  
+    """
+    Check that number of points is correct. 
+    """
 
-    interpolated_calibration = interpolate.interpn(converted_certificate, xi, method='cubic')
+    interpolated_calibration = interp(converted_certificate, xi, method='cubic')
+
+    PlotInterpolatedCalibration(converted_certificate,(xi,interpolated_calibration))
+
+    if(plot not OK ):
+       # Need to change some parameters if it doesn't look right
+       # Question is what and how.
+
+    """
+    Save the result to HDF file in Spectra->Spectrum n -> Spectra of calibration lamp -> Interpolated standard lamp calibration
+    """
+
+    location = ("Spectra->Spectrum n -> Spectra of calibration lamp -> Interpolated standard lamp calibration")
+
+    WriteToHDF(location,interpolated_calibration)   # Save the result somewhere in the HDF5 file
 
     return((xi,interpolated_calibration))     # Return a tuple of the interpolated calibration
     
@@ -40,3 +57,23 @@ def ConvertToPhotonsPerWavenumber(calibration_certificate):
     converted_certificate = calibration_certificate[1]/(wavenumbers^3)
 
     return((wavenumbers,converted_certificate))
+
+def PlotInterpolatedCalibration(converted_certificate,(xi,interpolated_calibration)):
+    
+    """
+    Plot the interpolated calibration on top of the original data. Verify it's OK
+    Where do we put the plot?
+    """
+
+    plt.plot(converted_certificate,label="Original data")     
+    """
+    converted certificate is a tuple. Make the original data a scatter plot
+    """
+    plt.plot(xi,interpolated_calibration,label="Interpolated data")  # Make this a line plot
+    plt.xlabel("Wavenumber (cm-1)")
+    plt.ylabel("Radiance (photons/(cm-1.cm^2.sr)")  # Scale units if necessary
+    plt.title("Interpolated radiance")y/n
+
+    message("OK y/n ")
+
+    return()
